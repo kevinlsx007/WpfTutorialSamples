32589ea91d533d271e37955675077af0dd6f01b9
Kevin Li <sli@enumerix.com>
2022-01-29 14:57:37 -0800

    Finished Chapter "The TabControl"

---
+++ Tab_control/ControllingTheTabControlSample.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfTutorialSamples.Tab_control
+{
+    /// <summary>
+    /// Interaction logic for ControllingTheTabControlSample.xaml
+    /// </summary>
+    public partial class ControllingTheTabControlSample : Window
+    {
+        public ControllingTheTabControlSample()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPreviousTab_Click(object sender, RoutedEventArgs e)
+        {
+            int newIndex = tcSample.SelectedIndex - 1;
+            if (newIndex < 0)
+                newIndex = tcSample.Items.Count - 1;
+            tcSample.SelectedIndex = newIndex;
+        }
+
+        private void btnNextTab_Click(object sender, RoutedEventArgs e)
+        {
+            int newIndex = tcSample.SelectedIndex + 1;
+            if (newIndex >= tcSample.Items.Count)
+                newIndex = 0;
+            tcSample.SelectedIndex = newIndex;
+        }
+
+        private void btnSelectedTab_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Selected tab: " + (tcSample.SelectedItem as TabItem).Header);
+        }
+    }
+}
---
+++ Tab_control/TabStripPlacementSample.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfTutorialSamples.Tab_control
+{
+    /// <summary>
+    /// Interaction logic for TabStripPlacementSampl.xaml
+    /// </summary>
+    public partial class TabStripPlacementSample : Window
+    {
+        public TabStripPlacementSample()
+        {
+            InitializeComponent();
+        }
+    }
+}
