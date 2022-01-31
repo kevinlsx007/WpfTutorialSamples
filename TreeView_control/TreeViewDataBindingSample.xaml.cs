518608e986a06945435373a3904b60a8fbd08375
Kevin Li <sli@enumerix.com>
2022-01-30 22:02:45 -0800

    Finished Chapter "The TreeView control"

---
+++ TreeView_control/TreeViewDataBindingSample.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using System.Collections.ObjectModel;
+
+namespace WpfTutorialSamples.TreeView_control
+{
+    /// <summary>
+    /// Interaction logic for TreeViewDataBindingSample.xaml
+    /// </summary>
+    public partial class TreeViewDataBindingSample : Window
+    {
+        public TreeViewDataBindingSample()
+        {
+            InitializeComponent();
+            MenuItem root = new MenuItem() { Title = "Menu" };
+            MenuItem childItem1 = new MenuItem() { Title = "Child item #1" };
+            childItem1.Items.Add(new MenuItem() { Title = "Child item #1.1" });
+            childItem1.Items.Add(new MenuItem() { Title = "Child item #1.2" });
+            root.Items.Add(childItem1);
+            root.Items.Add(new MenuItem() { Title = "Child item #2" });
+            trvMenu.Items.Add(root);
+        }
+    }
+    public class MenuItem
+    {
+        public MenuItem()
+        {
+            this.Items = new ObservableCollection<MenuItem>();
+        }
+
+        public string Title { get; set; }
+
+        public ObservableCollection<MenuItem> Items { get; set; }
+    }
+}
---
+++ TreeView_control/TreeViewMultipleTemplatesSample.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace WpfTutorialSamples.TreeView_control
+{
+    /// <summary>
+    /// Interaction logic for TreeViewMultipleTemplatesSample.xaml
+    /// </summary>
+    public partial class TreeViewMultipleTemplatesSample : Window
+    {
+        public TreeViewMultipleTemplatesSample()
+        {
+            InitializeComponent();
+
+            List<Family> families = new List<Family>();
+
+            Family family1 = new Family() { Name = "The Doe's" };
+            family1.Members.Add(new FamilyMember() { Name = "John Doe", Age = 42 });
+            family1.Members.Add(new FamilyMember() { Name = "Jane Doe", Age = 39 });
+            family1.Members.Add(new FamilyMember() { Name = "Sammy Doe", Age = 13 });
+            families.Add(family1);
+
+            Family family2 = new Family() { Name = "The Moe's" };
+            family2.Members.Add(new FamilyMember() { Name = "Mark Moe", Age = 31 });
+            family2.Members.Add(new FamilyMember() { Name = "Norma Moe", Age = 28 });
+            families.Add(family2);
+
+            trvFamilies.ItemsSource = families;
+        }
+    }
+    public class Family
+    {
+        public Family()
+        {
+            this.Members = new ObservableCollection<FamilyMember>();
+        }
+
+        public string Name { get; set; }
+
+        public ObservableCollection<FamilyMember> Members { get; set; }
+    }
+
+    public class FamilyMember
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
