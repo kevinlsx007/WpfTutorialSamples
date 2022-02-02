c8dd621c7c6fc250e94a4732469183af4e22a112
Kevin Li <sli@enumerix.com>
2022-02-01 21:42:06 -0800

    Finished Chapter "The DataGrid control"

---
+++ DataGrid_control/DataGridDetailsSample.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfTutorialSamples.DataGrid_control
+{
+    /// <summary>
+    /// Interaction logic for DataGridDetailsSample.xaml
+    /// </summary>
+    public partial class DataGridDetailsSample : Window
+    {
+        public DataGridDetailsSample()
+        {
+            InitializeComponent();
+            List<User> users = new List<User>();
+            users.Add(new User() { Id = 1, Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
+            users.Add(new User() { Id = 2, Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17) });
+            users.Add(new User() { Id = 3, Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
+
+            dgUsers.ItemsSource = users;
+        }
+    }
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public string Details
+        {
+            get
+            {
+                return String.Format("{0} was born on {1} and this is a long description of the person.", this.Name, this.Birthday.ToLongDateString());
+            }
+        }
+    }
+}
---
+++ DataGrid_control/SimpleDataGridSample.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfTutorialSamples.DataGrid_control
+{
+    /// <summary>
+    /// Interaction logic for SimpleDataGridSample.xaml
+    /// </summary>
+    public partial class SimpleDataGridSample : Window
+    {
+        public SimpleDataGridSample()
+        {
+            InitializeComponent();
+
+            List<User> users = new List<User>();
+            users.Add(new User() { Id = 1, Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
+            users.Add(new User() { Id = 2, Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17) });
+            users.Add(new User() { Id = 3, Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
+
+            dgSimple.ItemsSource = users;
+        }
+    }
+    //public class User
+    //{
+    //    public int Id { get; set; }
+
+    //    public string Name { get; set; }
+
+    //    public DateTime Birthday { get; set; }
+    //}
+}
